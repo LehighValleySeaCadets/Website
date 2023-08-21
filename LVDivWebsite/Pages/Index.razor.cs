6a1bd49b6d771576b164c8e2528c07cd3768a600
David Sweeney <david.j.sweeney@outlook.com>
2023-08-20 23:08:27 -0400

    Added carousel to index page.  More content still needed.  (#3)
    * Removed unused NavMenu component.  Added carousel to index razor page
    
    * Acknowleding source of getting carousel working.

---
+++ LVDivWebsite/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace LVDivWebsite.Pages;
+
+public partial class Index : ComponentBase
+{
+    const string carouselName = "mainCarousel";
+
+    [Inject]
+    public IJSRuntime? JSRuntime { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        _ = JSRuntime ?? throw new ArgumentNullException(nameof(JSRuntime));
+
+        object[] args = { carouselName };
+        await JSRuntime.InvokeVoidAsync("startCarousel", args);
+    }
+}
